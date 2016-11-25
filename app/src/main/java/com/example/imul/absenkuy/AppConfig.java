dac18bd2b1160a5d269cf6c7fc87b6cc28230b28
umi.sz <umishoima@gmail.com>
2016-11-25 17:07:56 +0700

    Commit project android

---
+++ app/src/main/java/com/example/imul/absenkuy/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.imul.absenkuy;
+
+/**
+ * Created by Imul on 11/12/2016.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.43.215/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.43.215/android_login_api/register.php";
+}
